cae15cc56e6bb96f65f2ca037a9b8c44d7fb4894
gianni <xlightningclawx110402@gmail.com>
2021-05-25 17:28:52 +0200

    eventueel klaar voor gebruik van deel 1 maar .dll doet raar, naar vragen

--- Linq2Xml/Linq2Xml/Program.cs
+++ Linq2Xml/Linq2Xml/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace Linq2Xml
 {
@@ -20,11 +21,58 @@ namespace Linq2Xml
     {
         public static void Main(string[] args)
         {
+            List<Cd> cds = createCds();
+            string testXml = TransformIntoXmlString(cds);
+            Console.WriteLine(testXml);
         }
 
-        public static string TransformIntoXmlString(Cd cd)
+        public static string TransformIntoXmlString(List<Cd> cds)
         {
-            return "";
+            var cdXml = new XDocument();
+            var rootElem = new XElement("CDs");
+            cdXml.Add(rootElem);
+            foreach (Cd cd in cds)
+            {
+                var cdElem = new XElement("cd");
+                var cdTitleElem = new XElement("title", cd.Title);
+                var cdArtistElem = new XElement("artist", cd.Artist);
+                
+                cdElem.Add(cdTitleElem);
+                cdElem.Add(cdArtistElem);
+
+                var tracksElem = new XElement("tracks");
+                foreach(Track track in cd)
+                {
+                    var trackTitleElem = new XElement("title", track.Title);
+                    var trackArtistElem = new XElement("artist", track.Artist);
+                    var trackLengthElem = new XElement("length", track.Length.ToString());
+                    var trackElem = new XElement("track");
+                    
+                    trackElem.Add(trackTitleElem);
+                    trackElem.Add(trackArtistElem);
+                    trackElem.Add(trackLengthElem);
+                    tracksElem.Add(trackElem);
+                }
+                
+                cdElem.Add(tracksElem);
+                rootElem.Add(cdElem);
+            }
+            return cdXml.ToString();
+        }
+
+        public static List<Cd> createCds()
+        {
+            Cd testCD = new Cd 
+            {
+                new Track { Title = "6:00", Artist = "Dream Theater", Length = new TimeSpan(0, 5, 31)},
+                new Track { Title = "Innocence Faded", Artist = "Dream Theater", Length = new TimeSpan(0, 5, 34)},
+                new Track { Title = "Scarred", Artist = "Dream Theater", Length = new TimeSpan(0, 10, 59)}
+            };
+            testCD.Title = "Awake";
+            testCD.Artist = "Dream Theater";
+
+            List<Cd> cds = new List<Cd> { testCD };
+            return cds;
         }
     }
 }
